33df2589187fde31a10126248e5bac398286ba96
va3wam <va3wam@gmail.com>
2021-11-01 17:00:10 -0400

    Move all header info from main.cpp to main.h Fixes #20

--- src/main.cpp
+++ src/main.cpp
@@ -1,51 +1,27 @@
-/******************************************************************************
- * @file main.cpp
- * 
- * @section intro_sec Introduction
- *
- * This Arduino for ESP32 code serves as the base code you will use to create 
- * your IOT embedded system's device software. Extend this code to create your 
- * application. This code is broken down into modules. 
- *
- * @section dependencies Dependencies
- * 
- * This class depends on on <a href="https://github.com/espressif/arduino-esp32">
- * Arduino.h</a> being present on your system. This is the Arduino core library
- * that comes bundled with PlatformIO.
- *
- * @section author Author(s)
- *
- * Written by Old Squire for the Aging Apprentice.
- *
- * @section license license
- *
- * Copyright 2021 the Aging Apprentice 
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to 
- * deal in the Software without restriction, including without limitation the 
- * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
- * sell copies of the Software, and to permit persons to whom the Software is 
- * furnished to do so, subject to the following conditions: The above copyright
- * notice and this permission notice shall be included in all copies or 
- * substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
- * IN THE SOFTWARE.  
- *****************************************************************************/
+/*
+   d8b          888     888               888                                                          
+   Y8P          888     888               888                                                          
+                888     888               888                                                          
+   888  .d8888b 888     888 88888b.   .d88888  .d88b.  888d888 888  888  888  8888b.  888d888  .d88b.  
+   888 d88P"    888     888 888 "88b d88" 888 d8P  Y8b 888P"   888  888  888     "88b 888P"   d8P  Y8b 
+   888 888      888     888 888  888 888  888 88888888 888     888  888  888 .d888888 888     88888888 
+   888 Y88b.    Y88b. .d88P 888  888 Y88b 888 Y8b.     888     Y88b 888 d88P 888  888 888     Y8b.     
+   888  "Y8888P  "Y88888P"  888  888  "Y88888  "Y8888  888      "Y8888888P"  "Y888888 888      "Y8888 
+
+   Log library for Arduino
+   https://github.com/theAgingApprentice/icUnderware
+   Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+ */
 #include <main.h> // Header file for linking.
 
 /**
- * @brief Initialize the serial output.
- * @details The usual Serial.print function used for serial output to the 
- * terminal is replaced in this project by the Arduino Log function. In the 
- * Log.begin() method pass one of the logging levels defined in Logging.h. 
- * Available levels are: LOG_LEVEL_SILENT, LOG_LEVEL_FATAL, LOG_LEVEL_ERROR, 
- * LOG_LEVEL_WARNING, LOG_LEVEL_INFO, LOG_LEVEL_TRACE or LOG_LEVEL_VERBOSE.
+ * Initialize the serial output. The usual Serial.print function used for 
+ * serial output to the terminal is replaced in this project by the Arduino 
+ * Log function. In the Log.begin() method pass one of the logging levels 
+ * defined in Logging.h. Available levels are: LOG_LEVEL_SILENT, 
+ * LOG_LEVEL_FATAL, LOG_LEVEL_ERROR, LOG_LEVEL_WARNING, LOG_LEVEL_INFO, 
+ * LOG_LEVEL_TRACE or LOG_LEVEL_VERBOSE.
+ * 
  * Note: To fully remove all logging code, uncomment the line 
  * #define DISABLE_LOGGING from the file Logging.h. This will significantly 
  * reduce the binary code file size.
--- src/main.h
+++ src/main.h
@@ -1,26 +1,38 @@
-/*******************************************************************************
- * @file main.h  
- *******************************************************************************/
+/*
+   d8b          888     888               888                                                          
+   Y8P          888     888               888                                                          
+                888     888               888                                                          
+   888  .d8888b 888     888 88888b.   .d88888  .d88b.  888d888 888  888  888  8888b.  888d888  .d88b.  
+   888 d88P"    888     888 888 "88b d88" 888 d8P  Y8b 888P"   888  888  888     "88b 888P"   d8P  Y8b 
+   888 888      888     888 888  888 888  888 88888888 888     888  888  888 .d888888 888     88888888 
+   888 Y88b.    Y88b. .d88P 888  888 Y88b 888 Y8b.     888     Y88b 888 d88P 888  888 888     Y8b.     
+   888  "Y8888P  "Y88888P"  888  888  "Y88888  "Y8888  888      "Y8888888P"  "Y888888 888      "Y8888 
+
+   Log library for Arduino
+   https://github.com/theAgingApprentice/icUnderware
+   Licensed under the MIT License <http://opensource.org/licenses/MIT>.
+ */
+
 #ifndef main_h // Start of precompiler check to avoid dupicate inclusion of this code block.
 
 #define main_h // Precompiler macro used for precompiler check.
 
-/*******************************************************************************
- * @section mainIncludes Included libraries.
- *******************************************************************************/
+/*
+ * Included libraries.
+ */
 #include <Arduino.h> // Arduino Core for ESP32. Comes with PlatformIO.
 #include <ArduinoLog.h> // https://github.com/thijse/Arduino-Log.
 #include <aaSocMicro.h> // Information about the host ESP32 SOC. 
 
-/************************************************************************************
- * @section mainGlobalVars Global variables, constants and objects.
- ************************************************************************************/
+/*
+ * Global variables, constants and objects.
+ */
 const unsigned long SERIAL_BAUD_RATE = 115200;
 aaSocMicro MCU;
 
-/************************************************************************************
- * @section mainDeclare Declare functions found in main.cpp.
- ************************************************************************************/
+/*
+ * Declare functions found in main.cpp.
+ */
 void setupSerial(); // Serial output initilization.
 void setup(); // Arduino mandatory function #1. Runs once at boot. 
 void loop(); // Arduino mandatory function #2. Runs continually.
