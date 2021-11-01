c4228c7b44c844f8b1f5775b046546709902d7b6
va3wam <va3wam@gmail.com>
2021-11-01 17:21:25 -0400

    Stop using README.md for Doxygen main page. Fixes #12

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,4 @@
-/*
-   d8b          888     888               888                                                          
-   Y8P          888     888               888                                                          
-                888     888               888                                                          
-   888  .d8888b 888     888 88888b.   .d88888  .d88b.  888d888 888  888  888  8888b.  888d888  .d88b.  
-   888 d88P"    888     888 888 "88b d88" 888 d8P  Y8b 888P"   888  888  888     "88b 888P"   d8P  Y8b 
-   888 888      888     888 888  888 888  888 88888888 888     888  888  888 .d888888 888     88888888 
-   888 Y88b.    Y88b. .d88P 888  888 Y88b 888 Y8b.     888     Y88b 888 d88P 888  888 888     Y8b.     
-   888  "Y8888P  "Y88888P"  888  888  "Y88888  "Y8888  888      "Y8888888P"  "Y888888 888      "Y8888 
-
-   Log library for Arduino
-   https://github.com/theAgingApprentice/icUnderware
-   Licensed under the MIT License <http://opensource.org/licenses/MIT>.
- */
-#include <main.h> // Header file for linking.
+#include <main.h> // Header file for linking
 
 /**
  * Initialize the serial output. The usual Serial.print function used for 
