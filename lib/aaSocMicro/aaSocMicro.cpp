9073e925f662764a5855b78d0dd5ecb06673d0dc
va3wam <va3wam@gmail.com>
2021-11-01 22:43:02 -0400

    Get private functions included in Doxygen output. Fixes #31 &  #13

--- lib/aaSocMicro/aaSocMicro.cpp
+++ lib/aaSocMicro/aaSocMicro.cpp
@@ -8,7 +8,7 @@
  * 1. Logging level = Silent. This means that this class will not send messages 
  * to the log.
  * 2. Logging output goes to the standard Serial interface. 
- * 3. Show logging level = TRUE. This prefixes a single letter to each log  
+ * 3. Show Logging level = TRUE. This prefixes a single letter to each log  
  * message that indicates the method used to issue it (e.g. Log.verbose() 
  * messages show up in the logs with a V prepended to them.
  * @param null
@@ -31,13 +31,13 @@ aaSocMicro::aaSocMicro()
  * 1. Logging level = Silent. This means that this class will not send messages 
  * to the log.
  * 2. Logging output goes wherever you specified with the output parameter. 
- * 3. Show logging level = TRUE. This prefixes a single letter to each log  
+ * 3. Show Logging level = TRUE. This prefixes a single letter to each log  
  * message that indicates the method used to issue it (e.g. Log.verbose() 
  * messages show up in the logs with a V prepended to them.
  * @param output class that handles bit stream input.
  * @return null
  ******************************************************************************/
-aaSocMicro::aaSocMicro(Print *output)
+aaSocMicro::aaSocMicro(Print* output)
 {
    bool showLevel = true; // Prefixed logging output with a single letter level.
    int loggingLevel = LOG_LEVEL_SILENT;
@@ -50,11 +50,11 @@ aaSocMicro::aaSocMicro(Print *output)
  * @details Instantiating this class using the third form results in you
  * controlling all Logging behavior for this class.
  * @param loggingLevel is one of 6 predefined levels from the Logging library.
- * @param output class that handles bit stream input.
- * @param showLevel when true prefixs log message with the Logging level letter.
+ * @param output is a class that can handle bit stream input (i.e. Serial).
+ * @param showLevel prefixs log message with the Logging level letter when TRUE.
  * @return null
  ******************************************************************************/
-aaSocMicro::aaSocMicro(int loggingLevel, Print *output, bool showLevel)
+aaSocMicro::aaSocMicro(int loggingLevel, Print* output, bool showLevel)
 {
    Log.begin(loggingLevel, output, showLevel); // Set logging parameters. 
    Log.traceln("<aaSocMicro::ThirdFormConstructor> Logging set to %d.", loggingLevel);
@@ -78,7 +78,7 @@ aaSocMicro::~aaSocMicro()
  * @param code is the nuerica reset value reported by the CPU. 
  * @return null 
  ******************************************************************************/
-void aaSocMicro::_transReasonCode(char &reason, RESET_REASON code)
+void aaSocMicro::_transReasonCode(char& reason, RESET_REASON code)
 {
    switch(code)
    {
@@ -283,7 +283,7 @@ void aaSocMicro::_logCoreMem()
  * @param null
  * @return null
  ******************************************************************************/
-void aaSocMicro::_transFlashModeCode(char &details)
+void aaSocMicro::_transFlashModeCode(char& details)
 {
    switch(ESP.getFlashChipMode())
    {
--- lib/aaSocMicro/aaSocMicro.h
+++ lib/aaSocMicro/aaSocMicro.h
@@ -1,3 +1,18 @@
+/*
+                   .d8888b.                    888b     d888 d8b                          
+                  d88P  Y88b                   8888b   d8888 Y8P                          
+                  Y88b.                        88888b.d88888                              
+ 8888b.   8888b.   "Y888b.    .d88b.   .d8888b 888Y88888P888 888  .d8888b 888d888 .d88b.  
+    "88b     "88b     "Y88b. d88""88b d88P"    888 Y888P 888 888 d88P"    888P"  d88""88b 
+.d888888 .d888888       "888 888  888 888      888  Y8P  888 888 888      888    888  888 
+888  888 888  888 Y88b  d88P Y88..88P Y88b.    888   "   888 888 Y88b.    888    Y88..88P 
+"Y888888 "Y888888  "Y8888P"   "Y88P"   "Y8888P 888       888 888  "Y8888P 888     "Y88P"  
+                                                                                                                                                  
+EP32 core control library for Arduino
+Github: https://github.com/theAgingApprentice/icUnderware/tree/main/lib/aaSocMicro
+Licensed under the MIT License <http://opensource.org/licenses/MIT>.                                                                                                              
+*/
+
 #ifndef aaSocMicro_h // Start precompiler code block. 
 
 #define aaSocMicro_h // Precompiler macro to prevent duplicate inclusions.
